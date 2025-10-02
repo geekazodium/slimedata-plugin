5adb62670dbb276117694b0a12f01d5ee36b55ca
geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-10-02 19:36:41 -0400

    feature: edit test scene

--- test/test_node.gd
+++ test/test_node.gd
@@ -1,6 +1,5 @@
 extends Node
 
 func _physics_process(_delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		print("ui accept pressed");
+	if Input.is_action_just_pressed("ui_focus_next"):
 		$"../FrameDataProvider".play_framedata("idle");
