cbadbb8f83ad4fd114bfd5c1bff6e15a7724b51b
geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-10-03 18:36:14 -0400

    notes: added notes on what I need to fix

--- addons/framedata/layers/hitbox/hitbox_layer.gd
+++ addons/framedata/layers/hitbox/hitbox_layer.gd
@@ -8,5 +8,7 @@ func _process_overlap(other_layer: FunctionBoxLayer,local: FunctionBoxShape, oth
 	if other_layer is HitBoxLayer: 
 		## FIXME: mark entire hurtboxs as ignored, not just local hitbox index
 		##var other_hurtbox_l = (other_layer as HitBoxLayer).get_framedata_provider().get_layer("HurtBoxLayer");
-		##self.set_as_handled(other_hurtbox_l, local.key);
+		##self.set_as_handled(other_hurtbox_l, local.key);	
+		## FIXME create way to set entire provider as handled instead in order
+		## to exclude self collisions and exclude this.
 		self.set_as_handled(other_layer, local.key);
