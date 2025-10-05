e866d36f9358caab79548485fd5f89bf6d52fa53
geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-10-05 18:46:23 -0400

    fix: buggy and inconsistient indexing behavior
    note to self: never write code when sleep deprived.

--- addons/attack_frame_data/editor_2d_view/toolspace/tool_space_utils.gd
+++ addons/attack_frame_data/editor_2d_view/toolspace/tool_space_utils.gd
@@ -2,22 +2,19 @@
 class_name ToolSpaceUtils
 
 static func add_shape(frame: FrameDataFrame, layer: int, shape: FunctionBoxShape) -> void:
-	var layer_count: int = frame.layer_start_idxs.size();
-	if layer + 1 >= layer_count:
-		frame.shapes.append(shape);
-		return;
-	
 	## optimization: order of items in array do not need to be kept, only order
 	## of partitions, o(n) and o(p) where n is the total size of array whereas p is the number of partitions
 	## the factor p is constant and defined as 3, therefore it can be argued this is O(1) time, and this
 	## lines up with observation from profiling.
-	var initial_size: int = frame.shapes.size();
+	
+	var layer_count: int = frame.layer_end_idxs.size();
 	var to_insert: FunctionBoxShape = shape;
-	var last_index: int = -1;
+	var last_index = -1;
 	for i in range(layer, layer_count):
-		var index: int = _get_shape_index(frame, i, initial_size);
-		if index == last_index:
+		var index: int = frame.get_layer_end(i);
+		if last_index == index:
 			continue;
+		last_index = index;
 		if index >= frame.shapes.size():
 			frame.shapes.append(to_insert);
 			break;
@@ -25,11 +22,5 @@ static func add_shape(frame: FrameDataFrame, layer: int, shape: FunctionBoxShape
 			var tmp = frame.shapes[index];
 			frame.shapes[index] = to_insert;
 			to_insert = tmp;
-	for i in range(layer + 1, layer_count):
-		frame.layer_start_idxs[i] += 1;
-
-static func _get_shape_index(frame: FrameDataFrame, layer: int, default: int) -> int:
-	if frame.layer_start_idxs.size() <= layer + 1:
-		return default;
-	else:
-		return frame.layer_start_idxs[layer + 1];
+	for i in range(layer, layer_count):
+		frame.layer_end_idxs[i] += 1;
