24618333bf27cba0f713a9988e12775e2b6f1033
geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-10-19 19:38:22 -0400

    feature: reserved index assignment

--- addons/slimedata/editor_2d_view/toolspace/tool_space_utils.gd
+++ addons/slimedata/editor_2d_view/toolspace/tool_space_utils.gd
@@ -62,4 +62,88 @@ static func add_shape_scale(shape: FunctionBoxShape, delta_r: float) -> void:
 		shape.radius = max(shape.radius, .1);
 
 static func save_optimized(provider: FrameDataProviderTool) -> void:
-	pass
+	reassign_index(provider.current_frame_data_cached);
+
+## reassigns indicies for all shapes in the frame data.
+static func reassign_index(frame_data: FrameData) -> void:
+	print("reassigning reserved indicies for ", frame_data);
+	var last_frame: FrameDataFrame = null;
+	
+	var last_frame_ids: Array[int];
+	var last_frame_id_partitions: PackedInt64Array = range(FrameDataProvider.DATA_LAYER_COUNT);
+	for frame in frame_data.frames:
+		if last_frame == null:
+			last_frame_ids = _update_frame_indicies_trivial(frame, last_frame_id_partitions);
+		else:
+			last_frame_ids = _update_frame_indices_with_last_frame(frame, last_frame_ids, last_frame_id_partitions);
+		last_frame = frame;
+
+static func _update_frame_indicies_trivial(frame: FrameDataFrame, id_partitions: PackedInt64Array) -> Array[int]:
+	var indicies: Array[int] = [];
+	for layer_index: int in range(FrameDataProvider.DATA_LAYER_COUNT):
+		var count: int = 0;
+		for i in frame.get_layer_range(layer_index):
+			frame.shapes[i].reserved_index = count;
+			indicies.append(count);
+			count += 1;
+	
+	## shortcut, given that there will not be any empty spaces,
+	## skip computing where each layer will end because the space does not change
+	for i in range(FrameDataProvider.DATA_LAYER_COUNT):
+		id_partitions[i] = frame.layer_end_idxs[i];
+	return indicies;
+
+static func _update_frame_indices_with_last_frame(frame: FrameDataFrame, last_frame_indexs: Array[int], id_partitions: PackedInt64Array) -> Array[int]:
+	const UNUSED_INDEX: int = -1;
+	
+	var indicies: Array[int] = [];
+	var new_layers_partition: Array[int] = [];
+	var new_partition_end: int = 0;
+	var last_layer_partition_end: int = 0;
+	
+	for layer_index: int in range(FrameDataProvider.DATA_LAYER_COUNT):
+		var count: int = 0;
+		var keys_shapes: Dictionary[int, Array] = _get_taken_keys(frame, layer_index);
+		## copy to existing indicies
+		for i in range(last_layer_partition_end, id_partitions[layer_index]):
+			var last_key: int = last_frame_indexs[i];
+			## if the keys that exists in this frame still has something ovelapping with
+			## the last, take that and write it there, otherwise add an unused
+			if keys_shapes.has(last_key):
+				var shapes: Array = keys_shapes.get(last_key);
+				var shape: FunctionBoxShape = shapes.pop_front();
+				shape.reserved_index = count;
+				
+				indicies.append(last_key);
+				if shapes.is_empty():
+					keys_shapes.erase(last_key);
+			else:
+				indicies.append(UNUSED_INDEX);
+			count += 1;
+			if keys_shapes.is_empty():
+				break;
+		
+		## add leftovers
+		for key: int in keys_shapes.keys():
+			var shapes: Array = keys_shapes.get(key);
+			for shape in shapes:
+				shape.reserved_index = count;
+				count += 1;
+			
+		last_layer_partition_end = id_partitions[layer_index];
+		
+		## increase partition size
+		new_partition_end += count;
+		new_layers_partition.append(new_partition_end);
+	
+	for i in range(FrameDataProvider.DATA_LAYER_COUNT):
+		id_partitions[i] = new_layers_partition[i];
+	return indicies;
+
+static func _get_taken_keys(frame: FrameDataFrame, layer_index: int) -> Dictionary[int, Array]:
+	var taken_keys: Dictionary[int, Array] = {};
+	for index: int in frame.get_layer_range(layer_index):
+		var shape: FunctionBoxShape = frame.shapes[index];
+		var arr: Array = taken_keys.get_or_add(shape.key, []);
+		arr.push_back(shape);
+	return taken_keys;
